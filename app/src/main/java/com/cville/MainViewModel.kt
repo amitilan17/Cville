c5961270a73ae4c332cc49ac40f4e2aa12c8ba8b
eiloncap <eiloncap@gmail.com>
2022-04-07 17:50:50 +0300

    merged with main

--- app/src/main/java/com/cville/MainViewModel.kt
+++ app/src/main/java/com/cville/MainViewModel.kt
@@ -1,31 +1,39 @@
 package com.cville
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class MainViewModel : ViewModel() {
-    var user = Firebase.auth.currentUser
+    lateinit var user: User
     private val db = FirebaseFirestore.getInstance()
 
     companion object {
         private const val USERS_COLLECTION_TAG = "Users"
     }
 
-//    fun uploadUser(item: User) {
-//        db.collection(USERS_COLLECTION_TAG)
-//            .document(user!!.uid)
-//            .set(item)
-//    }
-
-    fun loadUser() {
+    fun uploadUser(item: User) {
         db.collection(USERS_COLLECTION_TAG)
-            .document(user!!.uid)
+            .document(Firebase.auth.currentUser!!.uid)
+            .set(item)
+    }
+
+    fun loadMainUser() {
+        getUserObj(Firebase.auth.currentUser!!.uid) {
+            user = it
+        }
+    }
+
+    fun getUserObj(uid: String, callback: (User) -> Unit) {
+        db.collection(USERS_COLLECTION_TAG)
+            .document(Firebase.auth.currentUser!!.uid)
             .get()
             .addOnSuccessListener {
-                it.toObject(User::class.java)
+                val res = it.toObject(User::class.java)
+                if (res != null) {
+                    callback(res)
+                }
             }
             .addOnFailureListener {
             }
