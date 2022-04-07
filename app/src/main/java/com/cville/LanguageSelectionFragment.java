891b1a49801b4e402ee01fe1e9c1fb03ea8bca90
ayelet.shashaevron <ayelet.shashaevron@servicenow.com>
2022-04-07 17:59:54 +0300

    ma echpat lachem

--- app/src/main/java/com/cville/LanguageSelectionFragment.java
+++ app/src/main/java/com/cville/LanguageSelectionFragment.java
@@ -12,10 +12,10 @@ import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LanguageFragment#newInstance} factory method to
+ * Use the {@link LanguageSelectionFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LanguageFragment extends Fragment {
+public class LanguageSelectionFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class LanguageFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LanguageFragment() {
+    public LanguageSelectionFragment() {
         // Required empty public constructor
     }
 
@@ -39,8 +39,8 @@ public class LanguageFragment extends Fragment {
      * @return A new instance of fragment LanguageFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LanguageFragment newInstance(String param1, String param2) {
-        LanguageFragment fragment = new LanguageFragment();
+    public static LanguageSelectionFragment newInstance(String param1, String param2) {
+        LanguageSelectionFragment fragment = new LanguageSelectionFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
